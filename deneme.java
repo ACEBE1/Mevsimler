6a506cc04ae3be8c21330f5b300156ca835d90b2
ACEBE1 <75432685+ACEBE1@users.noreply.github.com>
2020-12-14 19:21:14 +0300

    Add files via upload

---
+++ deneme.java
@@ -0,0 +1,17 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class deneme {
+    public static void main(String[] args) {
+        System.out.println("Lütfen bir sayi giriniiz: ");
+        int sayi1 = new Scanner(System.in).nextInt();
+
+        System.out.println("Lütfen bir sayi giriniz: ");
+        int sayi2= new Scanner(System.in).nextInt();
+        if(sayi1>sayi2){{
+            System.out.println("Büyük olan sayi:"+sayi2);
+        }
+            System.out.println("Büyük olan sayi:"+sayi1);
+        }
+        else
+    }
+}
